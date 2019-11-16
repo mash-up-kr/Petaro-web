e236a1f985d7494baff544ea42f985ccabadeab3
aereeeee <dofl5576@gmail.com>
2019-11-16 16:29:41 +0900

    env: 초기 프로젝트 structure 세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     'react',
   ],
   rules: {
+    "react/jsx-filename-extension":"off",
   },
 };
---
+++ src/components/common/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const S = {
+  StyledButton: styled.button`
+  `,
+};
+
+const Button = () => (
+  <S.StyledButton />
+);
+
+export default Button;
